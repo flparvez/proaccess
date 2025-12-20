8ea055981ab234fffb6414b585157fd9afd19fa9
Md Parvez <flparvez23@gmail.com>
2025-12-20 17:19:54 +0600

    Upgrade SOme

--- src/components/admin/UserListClient.tsx
+++ src/components/admin/UserListClient.tsx
@@ -128,7 +128,7 @@ export default function UserListClient() {
     <div className="space-y-4">
       
       {/* --- HEADER CONTROLS --- */}
-      <div className="flex items-center gap-2 bg-[#111] p-4 rounded-xl border border-gray-800">
+      <div className="flex items-center gap-2 bg-[#111] p-1 rounded-xl border border-gray-800">
         <Search className="w-5 h-5 text-gray-500" />
         <Input
           placeholder="Search name, email, or phone..."
